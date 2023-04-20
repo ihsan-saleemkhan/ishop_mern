c2ec1a0a8bdcc1efd69ca2f46fd554ed0122ffdd
ihsan-saleemkhan <ihsan@mnmagency.com>
2023-04-20 04:04:52 +0300

    payment screen and save payment method

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -1,47 +1,72 @@
 import React from "react";
 import { Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { FaCheckCircle } from "react-icons/fa";
 
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
   return (
-    <Nav className="justify-content-center mb-4">
+    <Nav className="checkout-steps">
       <Nav.Item>
         {step1 ? (
           <LinkContainer to="/login">
-            <Nav.Link>Sign In</Nav.Link>
+            <Nav.Link>
+              <FaCheckCircle className="icon" /> Sign In
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Sign In</Nav.Link>
+          <div className="disabled">
+            <Nav.Link disabled>
+              <FaCheckCircle className="icon" /> Sign In
+            </Nav.Link>
+          </div>
         )}
       </Nav.Item>
 
       <Nav.Item>
         {step2 ? (
           <LinkContainer to="/shipping">
-            <Nav.Link>Shipping</Nav.Link>
+            <Nav.Link>
+              <FaCheckCircle className="icon" /> Shipping
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Shipping</Nav.Link>
+          <div className="disabled">
+            <Nav.Link disabled>
+              <FaCheckCircle className="icon" /> Shipping
+            </Nav.Link>
+          </div>
         )}
       </Nav.Item>
 
       <Nav.Item>
         {step3 ? (
           <LinkContainer to="/payment">
-            <Nav.Link>Payment</Nav.Link>
+            <Nav.Link>
+              <FaCheckCircle className="icon" /> Payment
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Payment</Nav.Link>
+          <div className="disabled">
+            <Nav.Link disabled>
+              <FaCheckCircle className="icon" /> Payment
+            </Nav.Link>
+          </div>
         )}
       </Nav.Item>
 
       <Nav.Item>
         {step4 ? (
           <LinkContainer to="/placeorder">
-            <Nav.Link>Place Order</Nav.Link>
+            <Nav.Link>
+              <FaCheckCircle className="icon" /> Place Order
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Place Order</Nav.Link>
+          <div className="disabled">
+            <Nav.Link disabled>
+              <FaCheckCircle className="icon" /> Place Order
+            </Nav.Link>
+          </div>
         )}
       </Nav.Item>
     </Nav>
